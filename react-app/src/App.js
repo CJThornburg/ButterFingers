d747027fc4c7b016a6893d6ac7e5194774798575
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-08-12 18:57:40 -0400

    add profilepage react route and component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,11 +3,12 @@ import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
+import ProfilePage from './components/Profile/ProfilePage'
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
 import TextPage from './components/Texts/TextPage'
 import { thunkGetAllTexts } from "./store/texts";
-import {thunkGetAllScores} from './store/scores'
+import { thunkGetAllScores } from './store/scores'
 
 function App() {
   const dispatch = useDispatch();
@@ -16,7 +17,7 @@ function App() {
     dispatch(authenticate())
       .then(() => setIsLoaded(true))
       .then(() => dispatch(thunkGetAllTexts()))
-      .then(() => dispatch(thunkGetAllScores()) )
+      .then(() => dispatch(thunkGetAllScores()))
 
   }, [dispatch]);
 
@@ -34,6 +35,10 @@ function App() {
           <Route path='/test'>
             <TextPage></TextPage>
           </Route>
+          <Route path='/user/:username'>
+            <ProfilePage></ProfilePage>
+          </Route>
+
         </Switch>
       )}
     </>
---
+++ react-app/src/components/Profile/ProfilePage/index.js
@@ -0,0 +1,17 @@
+
+import "./ProfilePage.css";
+
+function userNameCard() {
+
+
+  return (
+    <>
+    <h1>
+      rendering:
+      profilePage
+    </h1>
+    </>
+  );
+}
+
+export default userNameCard;
