45a01ae665a1bf276c76f87f130ed118f2a40eea
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-08-13 13:53:05 -0400

    add data and info presentaion in profile pages

--- react-app/src/components/Profile/ProfilePage/index.js
+++ react-app/src/components/Profile/ProfilePage/index.js
@@ -14,6 +14,7 @@ function ProfilePage() {
   // const texts = useSelector(state => Object.values(state.texts))
 
   const userObj = useSelector(state => Object.values(state.users).find(user => user.username.toLowerCase() === username.toLowerCase()))
+  const textObjs = useSelector(state => state.texts)
   const texts = useSelector(state => Object.values(state.texts))
   const scores = useSelector(state => Object.values(state.scores))
 
@@ -30,40 +31,51 @@ function ProfilePage() {
   const userScores = scores.filter((score => score.userId === userObj?.id))
   const totalExp = userScores.reduce((accumulator, currentValue) => accumulator + currentValue.runExp, 0);
   const totalMistakes = userScores.reduce((accumulator, currentValue) => accumulator + currentValue.mistakes, 0)
+
   const totalTime = userScores.reduce((accumulator, currentValue) => accumulator + currentValue.time, 0)
   const totalKpm = userScores.reduce((accumulator, currentValue) => accumulator + currentValue.kpm, 0);
   const averageKpm = totalKpm / userScores.length;
 
+  // for each score, search text and grab   characterCount noSpaceCharacterCount wordCount
+  let totalChars
+  let totalCharsNospace
+  let totalWords = 0
+  for (const score of scores) {
+    let currentTextId = score.textId
+    console.log(currentTextId)
+    totalChars += textObjs[currentTextId].characterCount
+    totalChars += textObjs[currentTextId].noSpaceCharacterCount
+    totalWords += textObjs[currentTextId].wordCount
+  }
+  console.log("totalWords", totalWords)
+
+
   // console.log(currentUser, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
   const userTexts = texts.filter((score => score.userId === userObj?.id))
-  console.log("user ran:", userScores.length, "tests")
-  console.log("user owns:", userTexts.length, "texts")
-  console.log("total exp", totalExp)
-  // console.log(totalMistakes)
-  // console.log(averageKpm)
-  const level = getLevel( totalExp)
 
-  console.log("level", level)
+  const level = getLevel(totalExp)
 
-  // in minutes, note to self, demo has a bunch of scores to their name
-  console.log((totalTime / 1000) / 60)
+
+
+
+  let totalTimeMin = (totalTime / 60000)
 
 
 
   // number of cards
 
-// checks i need
-          // current profile page is owned by current user
-                  // if so render whole page + delete profile button
-          // friendship exist at all
-                  // if exist and is active render whole page
-                  // if exist but is pending
-                          // if request is from current user render disabled pending button
-                          // if request is to current user render accept request button and decline button
-                                  // if accept, update record to "active"
-                                  // if rejected, delete record    or turn to rejected
-                  // if does not exist
-                        // render send request button, current user = fromUser, toUser is to profile page on
+  // checks i need
+  // current profile page is owned by current user
+  // if so render whole page + delete profile button
+  // friendship exist at all
+  // if exist and is active render whole page
+  // if exist but is pending
+  // if request is from current user render disabled pending button
+  // if request is to current user render accept request button and decline button
+  // if accept, update record to "active"
+  // if rejected, delete record    or turn to rejected
+  // if does not exist
+  // render send request button, current user = fromUser, toUser is to profile page on
 
 
 
@@ -80,16 +92,50 @@ function ProfilePage() {
           {userObj?.username}
         </p>
         <p className="PP-main-Profile-info PP-joined">
-           joined:  {userObj?.createdAt}
+          joined:  {userObj?.createdAt}
         </p>
         <p className="PP-main-Profile-info PP-level">
           Level: {level}
 
         </p>
+        {/* if friends render more stats */}
+        <p className="PP-tests">Tests Completed: {userScores.length}</p>
+        <p className="PP-kpm">Average KPM: {averageKpm.toFixed(2)}</p>
+        <p className="PP-time">Total time: {totalTimeMin.toFixed(2)}mins</p>
 
-{/* if not current user friends and no request sent render send request button */}
+
+
+
+
+        {/* div for mistakes, characters and noSpace characters */}
+
+
+
+
+        {/* if not current user friends and no request sent render send request button */}
         <div>
-        <
+
+        </div>
+
+        {/* if friends or current user */}
+        <div className="PP-stats2 PP-totals">
+          <p className="PP-mistakes"> typed characters: {totalMistakes}</p>
+          <p className="PP-mistakes">typed non-space characters: {totalCharsNospace}</p>
+          <p className="PP-mistakes">totals mistakes: {totalMistakes}</p>
+
+        </div>
+
+
+        {/* if friends or current user */}
+        <div>
+          {userTexts.map((text) => (
+            <>
+              <h4>{text.name}</h4>
+              {/* TODO add onclick to redirect to testing with this card :') */}
+              <button>run it!</button>
+            </>
+          ))}
+
         </div>
 
       </div>
