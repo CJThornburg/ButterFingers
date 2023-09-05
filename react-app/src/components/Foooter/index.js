6b854fa0ffba9cd40ca26b96c760d231830e0577
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-09-05 13:24:33 -0400

    email added

--- react-app/src/components/Foooter/index.js
+++ react-app/src/components/Foooter/index.js
@@ -10,6 +10,7 @@ function Footer() {
       <div className='footer-div'>
         <a className="footerLink yt anti-link" target='_blank' href="https://www.linkedin.com/in/chris-thornburg-swe/"> <i class="fa-brands  fa fa-linkedin"></i></a>
         <a className="footerLink yt anti-link" target='_blank' href="https://github.com/CJThornburg"> <i class="fa-brands fa fa-github"></i></a>
+        <a className="footerLink yt anti-link" target='_blank' href="mailto:cjthornburg@thornburgsfamily.com"> <i class="fa-solid fa fa-envelope"></i></a>
       </div>
       </>
     );
